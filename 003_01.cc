3e2db69283d0edacdcf19dc5172554a45f700cea
shnya <testmhigashi@gmail.com>
2011-06-17 01:00:44 +0900

    commit

--- 003_01.cc
+++ 003_01.cc
@@ -7,26 +7,26 @@ struct Answer_Is_ {};
 
 template<size_t n, size_t i, size_t div, size_t answer>
 struct _MaxPrime {
-  typedef typename _MaxPrime<n,i+1,n%(i+1),answer>::solution solution;
+  typedef typename _MaxPrime<n,i+1,n%(i+1),answer>::type type;
 };
 
 template<size_t n, size_t i, size_t answer>
 struct _MaxPrime<n,i,0,answer> {
-  typedef typename _MaxPrime<n/i,i,(n/i)%i,n>::solution solution;
+  typedef typename _MaxPrime<n/i,i,(n/i)%i,n>::type type;
 };
 
 template<size_t i, size_t answer>
 struct _MaxPrime<1,i,1,answer> {
-  typedef Answer_Is_<answer> solution;
+  typedef Answer_Is_<answer> type;
 };
 
 template<size_t n>
 struct MaxPrime{
-  typedef typename _MaxPrime<n,1,1,false>::solution solution;
+  typedef typename _MaxPrime<n,1,1,false>::type type;
 };
 
 int main(int argc, char *argv[])
 {
-  MaxPrime<TARGET>::solution a = 0;
+  MaxPrime<TARGET>::type a = 0;
   return 0;
 }
